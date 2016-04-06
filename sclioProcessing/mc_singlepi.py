6adbf7c7a64bd2eebefa323f245a281b0a11d980
syuvivida <syu@phy.ncu.edu.tw>
2016-04-06 15:28:38 +0800

    adding matched tracks

--- sclioProcessing/mc_singlepi.py
+++ sclioProcessing/mc_singlepi.py
@@ -17,7 +17,7 @@ import os
 
 # make list of files..
 import glob
-files=glob.glob("/home/syu/muon_collider/single_pion_analog/rfull005/pgun_pi1000gev*.slcio")
+files=glob.glob("/home/syu/muon_collider/single_pion_analog/rfull006/pgun_pi1000gev*.slcio")
 factory = LCFactory.getInstance()
 reader = factory.createLCReader()
 directory = 'dat'
@@ -27,13 +27,15 @@ else:
 	print 'the directory already exists! rememebr to clean up your work area'
 	quit()
 
-fileOutStatus1  = open('dat/of_status1.dat','w');
-fileOutStatus3  = open('dat/of_status3.dat','w');
-fileOutPanPFA   = open('dat/of_PanPFA.dat','w');
+fileOutStatus1         = open('dat/of_status1.dat','w');
+fileOutStatus3         = open('dat/of_status3.dat','w');
+fileOutPanPFA          = open('dat/of_PanPFA.dat','w');
 fileOutPanPFA_Tracks   = open('dat/of_PanPFA_Tracks.dat','w');
-fileOutPanPFA_CaloHits   = open('dat/of_PanPFA_CaloHits.dat','w');
-fileOutClusters = open('dat/of_CaloHits.dat','w');
-fileOutTracks   = open('dat/of_Tracks.dat','w');
+fileOutPanPFA_Calo     = open('dat/of_PanPFA_Calo.dat','w');
+fileOutClusters        = open('dat/of_CaloClusters.dat','w');
+fileOutTracks          = open('dat/of_Tracks.dat','w');
+fileOutECaloR          = open('dat/of_ECaloHits_r.dat','w');
+fileOutHCaloR          = open('dat/of_HCaloHits_r.dat','w');
 
 
 nEvent=0
@@ -46,63 +48,77 @@ for f in files:
 		nEvent=nEvent+1
 		# print " file event: ",evt.getEventNumber(), " run=",evt.getRunNumber()
 #		if (nEvent%100==0): print "# Event: ",nEvent
-		col = evt.getCollection("MCParticle")
-		colPF = evt.getCollection("PandoraPFOCollection");
-		colCl = evt.getCollection("ReconClusters");
-		colTr = evt.getCollection("Tracks");
-		nMc=col.getNumberOfElements()
+		col    = evt.getCollection("MCParticle")
+		colPF  = evt.getCollection("PandoraPFOCollection");
+		colCl  = evt.getCollection("ReconClusters");
+		colTr  = evt.getCollection("Tracks");
+		colECB = evt.getCollection("EcalBarrelHits");
+                colHCB = evt.getCollection("HcalBarrelHits");
+
+		nMc=col.getNumberOfElements();
 		nPF=colPF.getNumberOfElements();
 		nCl=colCl.getNumberOfElements();
-		nTr=colTr.getNumberOfElements();
+		nTr=colTr.getNumberOfElements();                
+		nECB=colECB.getNumberOfElements();		
+		nHCB=colHCB.getNumberOfElements();
+
 #		print " ----------- ------------ ",nEvent
 #		print "number of particles = ", nMc, nPF, nTr
 		fileOutStatus1.write( '-99 0 0 0 0 \n' );
 		fileOutStatus3.write( '-99 0 0 0 0 \n' );
 		fileOutPanPFA.write( '-99 0 0 0 0 \n' );
-		fileOutPanPFA_CaloHits.write( '-99 0 0 0 0 \n' );
+		fileOutPanPFA_Calo.write( '-99 0 0 0 0 \n' );
 		fileOutPanPFA_Tracks.write( '-99 0 0 0 0 \n' );
 		fileOutClusters.write( '-99 0 0 0 0 \n' );
 		fileOutTracks.write( '-99 0 0 0 0 \n' );
+                fileOutECaloR.write( '-99 0 0 0 0 \n' ); 
+                fileOutHCaloR.write( '-99 0 0 0 0 \n' ); 
+
 		for i in range(nMc): # loop over all particles 
 			par=col.getElementAt(i)
-			if par.getGeneratorStatus() == 3: 
-				pdg=par.getPDG();
-				charge=par.getCharge();
-				if par.getParents().size() > 0:
-					if math.fabs(par.getParents()[0].getPDG()) == 24 or math.fabs(par.getPDG()) == 24 or par.getPDG() > -999999:
-						print i, "pdg=",pdg, ", status=",par.getGeneratorStatus(), " mass=",par.getMass(), " mother=",par.getParents()[0].getPDG(), " momsize=",par.getMomentum()[0],par.getMomentum()[1],par.getMomentum()[2],par.getEnergy(); 
-						fileOutStatus3.write( '{0:3} {1:4.4f} {2:4.4f} {3:4.4f} {4:4.4f} \n'.format(par.getPDG(),par.getMomentum()[0],par.getMomentum()[1],par.getMomentum()[2],par.getEnergy()) )
 			if par.getGeneratorStatus() == 1: 
-				if (math.fabs(par.getPDG()) == 12 or math.fabs(par.getPDG()) == 14 or math.fabs(par.getPDG()) == 16): continue;
-				fileOutStatus1.write( '{0:3} {1:4.4f} {2:4.4f} {3:4.4f} {4:4.4f} \n'.format(par.getPDG(),par.getMomentum()[0],par.getMomentum()[1],par.getMomentum()[2],par.getEnergy()) )
+                            fileOutStatus1.write( '{0:3} {1:4.4f} {2:4.4f} {3:4.4f} {4:4.4f} \n'.format(par.getPDG(),par.getMomentum()[0],par.getMomentum()[1],par.getMomentum()[2],par.getEnergy()) )
 		
 		for i in range(nPF): # loop over all particles
 			parPF=colPF.getElementAt(i) 
 			fileOutPanPFA.write( '{0:3} {1:4.4f} {2:4.4f} {3:4.4f} {4:4.4f} \n'.format(parPF.getType(),parPF.getMomentum()[0],parPF.getMomentum()[1],parPF.getMomentum()[2],parPF.getEnergy()) )
-			## Look for CaloClusters associated with PF			
-			ECluster = -99;
-			if parPF.getClusters().size() > 0: 
-				for j in range(parPF.getClusters()[0].getCalorimeterHits().size()):
-					fileOutPanPFA_CaloHits.write( '{0:3} {1:4.4f} {2:4.4f} {3:4.4f} {4:4.4f} \n'.format(parPF.getClusters()[0].getCalorimeterHits()[j].getType(),parPF.getClusters()[0].getCalorimeterHits()[j].getPosition()[0],parPF.getClusters()[0].getCalorimeterHits()[j].getPosition()[1],parPF.getClusters()[0].getCalorimeterHits()[j].getPosition()[2],parPF.getClusters()[0].getCalorimeterHits()[j].getEnergy()) )
-			if parPF.getTracks().size() > 1:
-				print i, parPF.getTracks().size()
 
-#			if parPF.getTracks().size() > 0:
-#				for k in range(parPF.getTracks().size()):
-#					fileOutPanPFA_Tracks.write( '{0:3} {1:4.4f} {2:4.4f} {3:4.4f} {4:4.4f} \n'.format(parPF.getTracks()[k].getType(),parPF.getTracks()[k].getMomentum()[0],parPF.getTracks()[k].getMomentum()[1],parPF.getTracks()[k].getMomentum()[2],parPF.getTracks()[k].getEnergy()))
-					
+			## Look for CaloClusters associated with PF			
+			if parPF.getClusters().size() > 1:
+                                print "clusters: ", nEvent, i, parPF.getClusters().size()                                
+			if parPF.getClusters().size() > 0: 
+                            fileOutPanPFA_Calo.write( '{0:3} {1:4.4f} {2:4.4f} {3:4.4f} {4:4.4f} \n'.format(parPF.getClusters()[0].getType(),parPF.getClusters()[0].getPosition()[0],parPF.getClusters()[0].getPosition()[1],parPF.getClusters()[0].getPosition()[2],parPF.getClusters()[0].getEnergy()) )
+
+                        ## Look for tracks associated with PF 
+			if parPF.getTracks().size() > 1:
+                                print nEvent, i, parPF.getTracks().size()                                
+			if parPF.getTracks().size() > 0:
+                                trackpt  = math.fabs( (3e-4*5/parPF.getTracks()[0].getOmega()));
+                                cotTheta = parPF.getTracks()[0].getTanLambda();
+                                trackp   = trackpt * math.sqrt(1 + cotTheta*cotTheta );
+                                phi      = parPF.getTracks()[0].getPhi();
+                                trackpx  = trackpt*math.cos(phi);
+                                trackpy  = trackpt*math.sin(phi);
+                                trackpz  = trackpt*cotTheta;
+                                trackE   = math.sqrt(trackp*trackp + 0.13957*0.13957)
+
+                                fileOutPanPFA_Tracks.write( '{0:3} {1:4.4f} {2:4.4f} {3:4.4f} {4:4.4f} \n'.format(parPF.getTracks()[0].getType(),trackpx,trackpy,trackpz,trackE))
+
+                # print out calorimeter hits
 		for i in range(nCl):
 			parCl = colCl.getElementAt(i);
-			# print "position = ",len(parCl.getPosition()), parCl.getPosition()[0], parCl.getPosition()[1], parCl.getPosition()[2], parCl.getEnergy();
-			# perp2 = parCl.getPosition()[0]*parCl.getPosition()[0] + parCl.getPosition()[1]*parCl.getPosition()[1]
-			# print "phi = ", math.atan2(parCl.getPosition()[1],parCl.getPosition()[0]), "and theta = ", math.atan2(math.sqrt(perp2),parCl.getPosition()[2])
 			fileOutClusters.write( '{0:3} {1:4.4f} {2:4.4f} {3:4.4f} {4:4.4f} \n'.format(parCl.getType(),parCl.getPosition()[0],parCl.getPosition()[1],parCl.getPosition()[2],parCl.getEnergy()) )
 
-		# for i in range(nTr):
-		# 	parTr = colTr.getElementAt(i);
-		# 	fileOutTracks.write( '{0:3} {1:4.4f} {2:4.4f} {3:4.4f} {4:4.4f} \n'.format(parTr.getType(),parTr.getMomentum()[0],parTr.getMomentum()[1],parTr.getMomentum()[2],parTr.getEnergy()) )
+#		for i in range(nECB):
+#			parCl = colECB.getElementAt(i);
+#			fileOutECaloR.write( '{0:3} {1:4.4f} {2:4.4f} {3:4.4f} {4:4.4f} \n'.format(0,parCl.getPosition()[0],parCl.getPosition()[1],parCl.getPosition()[2],parCl.getEnergy()) )
 
-#		if nEvent > 1000: break;
+#		for i in range(nHCB):
+#			parCl = colHCB.getElementAt(i);
+#			fileOutHCaloR.write( '{0:3} {1:4.4f} {2:4.4f} {3:4.4f} {4:4.4f} \n'.format(1,parCl.getPosition()[0],parCl.getPosition()[1],parCl.getPosition()[2],parCl.getEnergy()) )
+
+
+#		if nEvent > 10: break;
 reader.close() # close the file
 
 
