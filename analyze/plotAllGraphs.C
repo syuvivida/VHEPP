cc489b10182592bc7061a84ef776487a065a9893
syuvivida <syu@phy.ncu.edu.tw>
2016-07-11 16:30:14 +0200

    update fit function

--- analyze/plotAllGraphs.C
+++ analyze/plotAllGraphs.C
@@ -1,5 +1,6 @@
 #include <string>
 #include "setNCUStyle.C"
+#include "TF1.h"
 
 using namespace std;
 void plotAllGraphs()
@@ -7,6 +8,7 @@ void plotAllGraphs()
   setNCUStyle(true);
  
   TCanvas* c1 = new TCanvas("c1","",600,600);
+  
   gr_RMS->Draw();
   c1->Print("RMS.pdf");
   c1->Print("RMS.gif");
@@ -36,6 +38,30 @@ void plotAllGraphs()
   c1->Print("GausSigma.pdf");
   c1->Print("GausSigma.gif");
   c1->Clear();
+
+
+  gStyle->SetOptFit(11111);
+
+  TF1* f1 = new TF1("f1","[0]/sqrt(x)+[1]");
+  f1->SetNpx(2500);
+  h_RMS->Fit("f1");
+  c1->Print("RMSFit.pdf");
+  c1->Print("RMSFit.gif");
+  c1->Clear();
+
+
+  TF1* f2 = new TF1("f2","[0]*(1-[1]*exp(-x/[2]))");
+  f2->SetNpx(2500);
+  f2->SetParameters(1,1,5000);
+  h_Mean->Fit("f2");
+  gStyle->SetStatX(0.90);
+  gStyle->SetStatY(0.37);
+  h_Mean->Fit("f2");
+
+  c1->Print("MeanFit.pdf");
+  c1->Print("MeanFit.gif");
+  c1->Clear();
+
   
 
 }
