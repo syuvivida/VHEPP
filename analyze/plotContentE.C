a165c0e5bbd3b42870c2f30d0a70eed955b92ce5
syuvivida <syu@phy.ncu.edu.tw>
2016-07-30 11:33:48 +0200

    macro to plot particle energy distributions

--- analyze/plotContentE.C
+++ analyze/plotContentE.C
@@ -8,9 +8,9 @@
 #include <TLegend.h>
 
 using namespace std;
-void plotJetContent(std::string inputFile)
+void plotContentE(std::string inputFolder)
 {
-  setNCUStyle(true);
+  setNCUStyle();
   std::map<unsigned int, std::string> PDGIDs;
 
   const int nparts=2300;
@@ -18,16 +18,16 @@ void plotJetContent(std::string inputFile)
   typedef std::map<unsigned int,std::string>::iterator it_type;
 
   PDGIDs[22]  = "pho";
-  PDGIDs[11]  = "ele";
-  PDGIDs[13]  = "muo";
+//   PDGIDs[11]  = "ele";
+//   PDGIDs[13]  = "muo";
   PDGIDs[211] = "pion";
   PDGIDs[321] = "kaon";
   PDGIDs[130] = "KL";
-  PDGIDs[310] = "Ks";
+//   PDGIDs[310] = "Ks";
   PDGIDs[2212] = "p";
   PDGIDs[2112] = "n";
 
-  TGraphErrors *gtemp[10];
+  TH1F *gtemp[10];
 
   int COLOR[2300];
   COLOR[22] = 2;
@@ -51,47 +51,48 @@ void plotJetContent(std::string inputFile)
   MARKER[2212] = 29;
   MARKER[2112] = 21;
 
-  TFile *f = TFile::Open(inputFile.data());
+  TFile *f = TFile::Open(Form("%s/radius0.4_jetContent.root",inputFolder.data()));
  
   TCanvas* c1 = new TCanvas("c1","",600,600);
   
   int i=0;
+  c1->SetLogy(1);
+  TLegend* leg = new TLegend(0.3,0.74,0.931,0.89);
+  leg-> SetNColumns(2);
+  leg->SetFillColor(0);
+  leg->SetFillStyle(0);
+  leg->SetTextSize(0.05);
 
-  TMultiGraph *mg = new TMultiGraph();
+  float max=-9999;
 
   for(it_type iterator = PDGIDs.begin(); iterator != PDGIDs.end(); iterator++) {
     
-    gtemp[i] = dynamic_cast<TGraphErrors*>(gDirectory->Get(Form("gr_Mean_%s",iterator->second.data())));
+    gtemp[i] = dynamic_cast<TH1F*>(gDirectory->Get(Form("he_%s",iterator->second.data())));
     gtemp[i]->SetMarkerColor(COLOR[iterator->first]);
     gtemp[i]->SetLineColor(COLOR[iterator->first]);
     gtemp[i]->SetMarkerStyle(MARKER[iterator->first]);
     gtemp[i]->SetMarkerSize(1.2);
     gtemp[i]->SetName(Form("gr_Mean_%s",iterator->second.data()));
-    mg->Add(gtemp[i]);
-
+    gtemp[i]->GetXaxis()->SetTitle("E_{true} [GeV]");
+    gtemp[i]->Rebin(5);
+    gtemp[i]->GetXaxis()->SetRangeUser(0,500);
+    gtemp[i]->GetYaxis()->SetTitle("Normalized distributions");
+    gtemp[i]->GetYaxis()->SetTitleOffset(1.4);
+    if(gtemp[i]->GetMaximum()>max)max=gtemp[i]->GetMaximum();
+    if(i==0)
+      gtemp[i]->DrawNormalized("hist");
+    else
+      gtemp[i]->DrawNormalized("same,hist");
+    leg->AddEntry(Form("gr_Mean_%s",iterator->second.data()),iterator->second.data(), "l");  
     std::string tempName =gtemp[i]->GetName();
     i++;
     
   }
+  gtemp[0]->SetMaximum(max*1.2);
 
-
-  mg->Draw("AP");
-  mg->GetXaxis()->SetNdivisions(5);
-  mg->GetXaxis()->SetTitle("E_{true} [GeV]");
-  mg->GetYaxis()->SetRangeUser(0,0.6);
-  mg->GetYaxis()->SetTitle("Mean of E_{particle}/E_{true}");
-
-  TLegend* leg = new TLegend(0.156,0.763,0.931,0.912);
-  leg-> SetNColumns(3);
-  leg->SetFillColor(0);
-  leg->SetFillStyle(0);
-  leg->SetTextSize(0.05);
-  for(it_type iterator = PDGIDs.begin(); iterator != PDGIDs.end(); iterator++) {
-    leg->AddEntry(Form("gr_Mean_%s",iterator->second.data()),iterator->second.data(), "p");  
-  }
 
   leg->Draw("same");
-  c1->Print("genJetContent.gif");  
-  c1->Print("genJetContent.pdf");
+  c1->Print(Form("%s/genContentE.gif",inputFolder.data()));  
+  c1->Print(Form("%s/genContentE.pdf",inputFolder.data()));  
 
 }
